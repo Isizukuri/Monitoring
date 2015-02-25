2225614a9916704999d420f55c1f56c6f11703a5
Isizukuri <ragnarok49@gmail.com>
2015-02-25 14:44:11 +0200

    Змінено парсер на lxml

--- Monitoring.py
+++ Monitoring.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from BeautifulSoup import BeautifulStoneSoup
 from lxml import html
-SearchWord=raw_input('Enter key word: ')
-StartDate = raw_input('Enter start of search period: ')
-EndDate = raw_input('Enter start of search period: ')
+SearchWord='прокуратура'
+StartDate = '16.02.2015'
+EndDate = '20.02.2015'
 Adress='http://www.reyestr.court.gov.ua/'
 StartSearchParam={'SearchExpression': SearchWord,'CourtRegion[]':'18','CourtName[]':'166','UserCourtCode':'','ChairmanName':'','RegNumber':'','RegDateBegin': StartDate,'RegDateEnd': EndDate,'CSType[]':'4','CSType[]':'3','CSType[]':'1', 'CaseNumber':'','ImportDateBegin':'','ImportDateEnd':'','Sort':'0','PagingInfo.ItemsPerPage':'25','Liga':'false',}
 StartSearch=requests.post('http://www.reyestr.court.gov.ua/',data=StartSearchParam)
@@ -15,10 +15,8 @@ for link in SoupSearchResult.find_all('a'):
 while (None in LinkList):
     LinkList.remove(None)
 CourtLink=Adress[:-1]+LinkList[6]
-SoupAdjudication=BeautifulSoup((requests.get(CourtLink)).text)
-
-
-print Adjudication.get_text()
+SoupAdjudication=BeautifulSoup((requests.get(CourtLink)).text, 'xml')
+print SoupAdjudication.div.get_text()
 #File=open('/home/nebo/Monitoring/2.html','w')
 #File.write(str(Adjudication))
 
