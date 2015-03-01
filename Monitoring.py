20115ff15c5aba9d6ec765dc16fdb68494ff6994
Isizukuri <ragnarok49@gmail.com>
2015-03-01 21:55:43 +0200

    modified:   Monitoring.py Геть переписаний код, з колосальною допомогою Volodya Yan. Тепер програма витягує рішення за вказаний період часу і пише їх у файли, які містяться у відповідні папці. Все потребує вдосконалення, дуже сира програма.

--- Monitoring.py
+++ Monitoring.py
@@ -1,23 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+from os import mkdir as mkdir
+from os import chdir as chdir
 import requests
-from bs4 import BeautifulSoup
-from BeautifulSoup import BeautifulStoneSoup
-from lxml import html
-SearchWord='прокуратура'
-StartDate = '16.02.2015'
-EndDate = '20.02.2015'
-Adress='http://www.reyestr.court.gov.ua/'
-StartSearchParam={'SearchExpression': SearchWord,'CourtRegion[]':'18','CourtName[]':'166','UserCourtCode':'','ChairmanName':'','RegNumber':'','RegDateBegin': StartDate,'RegDateEnd': EndDate,'CSType[]':'4','CSType[]':'3','CSType[]':'1', 'CaseNumber':'','ImportDateBegin':'','ImportDateEnd':'','Sort':'0','PagingInfo.ItemsPerPage':'25','Liga':'false',}
-StartSearch=requests.post('http://www.reyestr.court.gov.ua/',data=StartSearchParam)
-SoupSearchResult=BeautifulSoup(StartSearch.text)
-LinkList=[]
-for link in SoupSearchResult.find_all('a'):
-    LinkList.append(link.get('href'))
-while (None in LinkList):
-    LinkList.remove(None)
-CourtLink=Adress[:-1]+LinkList[6]
-SoupAdjudication=BeautifulSoup((requests.get(CourtLink)).text, 'xml')
-print SoupAdjudication.div.get_text()
-#File=open('/home/nebo/Monitoring/2.html','w')
-#File.write(str(Adjudication))
+from BeautifulSoup import BeautifulSoup as BS
+from urlparse import urljoin
 
+start_date = '16.02.2015'
+end_date = '20.02.2015'
 
+url = 'http://www.reyestr.court.gov.ua/'
+data = {'SearchExpression': 'прокуратура',
+        'CourtRegion[]':'18',
+        'CourtName[]':'166',
+        'RegDateBegin': start_date,
+        'RegDateEnd': end_date,
+        'CSType[]':'1',
+        'PagingInfo.ItemsPerPage':'50',
+        'Liga':'false'}
+
+response = requests.post(url, data)
+if response.status_code != 200:
+    raise RuntimeError('Got unexpected response', response)
+
+soup = BS(response.text)
+res_table = soup.find('table', id='tableresult')
+rows = res_table.findAll('tr')[1:]
+rel_links = [ row.find('td').a['href'] for row in rows ]
+
+path = ('./'+start_date+'-'+end_date)
+#mkdir(path)
+chdir(path)
+
+for link in rel_links:
+    time.sleep(2)
+    path = ('./'+start_date+'-'+end_date)
+    child_page = BS(requests.get(urljoin(url, link)).text)
+    text = child_page.body.find('textarea', id='txtdepository').string # contents[0]
+    soup_text = BS(text)
+    with open(link[9:]+'.html', 'w') as f:
+        f.write(text.encode('utf-8'))
